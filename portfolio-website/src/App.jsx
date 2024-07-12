c30ff28a127679c4596a9250b687fbc290941a9f
Pranay <pranayjalan.work@gmail.com>
2024-07-12 13:58:49 +0530

    first commit

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import boxicons from "node_modules/boxicons";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio-website/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container"></nav>
+    </header>
+  );
+};
+
+export default Header;
