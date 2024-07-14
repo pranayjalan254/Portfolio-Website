4e5fc04e68df6e4dc40fa86521b61906721560d1
Pranay <pranayjalan.work@gmail.com>
2024-07-14 17:49:53 +0530

    d

--- portfolio-website/src/components/home/Social.jsx
+++ portfolio-website/src/components/home/Social.jsx
@@ -7,21 +7,21 @@ const Social = () => {
       className="home__social-icon"
       target="_blank"
     >
-      <i class="uil uil-instagram"></i>
+      <i className="uil uil-instagram"></i>
     </a>
     <a
       href="https://www.linkedin.com/in/pranay-jalan254"
       className="home__social-icon"
       target="_blank"
     >
-      <i class="uil uil-linkedin-alt"></i>
+      <i className="uil uil-linkedin-alt"></i>
     </a>
     <a
       href="https://github.com/pranayjalan254"
       className="home__social-icon"
       target="_blank"
     >
-      <i class="uil uil-github-alt"></i>
+      <i className="uil uil-github-alt"></i>
     </a>
   </div>;
 };
