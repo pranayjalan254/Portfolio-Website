4bca44e945fc2a67c08ae3dd2e55c4056d03f1c3
Pranay <pranayjalan.work@gmail.com>
2024-07-15 19:14:46 +0530

    completed home

--- portfolio-website/src/components/home/ScrollDown.jsx
+++ portfolio-website/src/components/home/ScrollDown.jsx
@@ -39,6 +39,8 @@ const ScrollDown = () => {
             }}
           ></path>
         </svg>
+        <span className="home__scroll-name">Scroll Down</span>
+        <i className="uil uil-arrow-down home__scroll-arrow"></i>
       </a>
     </div>
   );
