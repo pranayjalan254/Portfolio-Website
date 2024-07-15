8b182cded1cd60219eed49e27a4fb17442bace0c
Pranay <pranayjalan.work@gmail.com>
2024-07-15 21:44:16 +0530

    complete about section

---
+++ portfolio-website/src/components/about/Info.jsx
@@ -0,0 +1,22 @@
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="uil uil-analytics"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Fresher</span>
+      </div>
+      <div className="about__box">
+        <i class="uil uil-github-alt"></i>
+        <h3 className="about__title">GitHub</h3>
+        <span className="about__subtitle">pranayjalan254</span>
+      </div>
+      <div className="about__box">
+        <i class="uil uil-envelope-question"></i>
+        <h3 className="about__title">Contact</h3>
+        <span className="about__subtitle">Email 24/7</span>
+      </div>
+    </div>
+  );
+};
+export default Info;
