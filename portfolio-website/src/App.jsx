87203f3ae4e7b087f59fb8b2b50f8c78aba2e2ad
Pranay <pranayjalan.work@gmail.com>
2024-07-16 16:31:25 +0530

    contact form

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </main>
     </>
   );
