a8e95497ec2771019be75c0b43b9841fe873c72a
Pranay <pranayjalan.work@gmail.com>
2024-07-21 19:56:23 +0530

    update

--- portfolio-website/src/components/coursework/Coursework.jsx
+++ portfolio-website/src/components/coursework/Coursework.jsx
@@ -7,21 +7,23 @@ const Coursework = () => {
     "Database Management Systems",
     "Object-Oriented Programming",
     "Digital Design",
-    "Microprocessors and interfacing",
+    "Microprocessors and Interfacing",
     "Microelectronics Circuits",
   ];
 
   return (
-    <div className="coursework-container">
-      <h2 className="section__title">Relevant Coursework</h2>
-      <div className="coursework-grid">
-        {subjects.map((subject, index) => (
-          <div key={index} className="coursework-card">
-            <p>{subject}</p>
-          </div>
-        ))}
+    <section className="section coursework">
+      <div className="coursework-container">
+        <h2 className="section__title">Relevant Coursework</h2>
+        <div className="coursework-grid">
+          {subjects.map((subject, index) => (
+            <div key={index} className="coursework-card">
+              <p>{subject}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default Coursework;
